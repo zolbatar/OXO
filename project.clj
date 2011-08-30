9dceecd3f7484a3308405b3c564c7f83e9c6ba9e
Daryl Dudey <daryljdudey@googlemail.com>
2011-08-30 14:08:09 +0100

    Initial states generated

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject OXO "1.0.0-SNAPSHOT"
   :description "A clojure based noughts and crosses genetic algorithm"
-  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
   :main OXO.core)
--- src/OXO/core.clj
+++ src/OXO/core.clj
@@ -1,12 +1,18 @@
 (ns OXO.core
+  (:require [clojure.contrib.math :as math]
+            [clojure.contrib.string :as string]
+            [clojure.contrib.pprint :as pprint])
   (:gen-class))
 
 (defrecord State [board moves])
 
 (defn create-states []
-  (dotimes [x (expt 3 9)]
-    (println x)
-  ))
+  (for [x (range (math/expt 3 9))]
+    (string/replace-char \0 \.
+          (string/replace-char \1 \O
+          (string/replace-char \2 \X (str (pprint/cl-format nil "~3,9,'0r" x)))))))
 
 (defn -main [& args]
-  (create-states))
+  (time
+    (let [states (create-states)]
+      (println (str (count states) " possible game states")))))
